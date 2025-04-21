2ae7c88b30599de6ee37785be711623ddde6fdcb
shahriar10rifat <shahriar7rifat@gmail.com>
2025-04-21 09:41:09 +0600

    app structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
-import MovieCard from './components/MovieCard';
  
-
 function App() {
-
   return (
     <>
-      <MovieCard movie={{title : "Shahs Film" , release_date: "2021"}} />
+      
     </>  
   )
 }
