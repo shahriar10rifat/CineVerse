9ee48a04e3d55f1b6134edfacdb1932711a63758
shahriar10rifat <shahriar7rifat@gmail.com>
2025-05-02 06:57:50 +0600

    react router added

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite movies yet</h2>
+      <p>Start adding to favorites!</p>
+    </div>
+  );
+}
+
+export default Favorite;
